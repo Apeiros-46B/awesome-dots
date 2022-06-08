3138b2acaaa6766e14698b93eb9230638f2c1f02
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2022-06-08 09:27:22 -0700

    Clean up analog clock code, add comments and folds

--- widgets/analog_clock.lua
+++ widgets/analog_clock.lua
@@ -1,105 +1,119 @@
+-- {{{ Library imports
+-- Core libraries
 local gears = require("gears")
 local wibox = require("wibox")
 
+-- Cairo and lua's math
 local cairo = require("lgi").cairo
 local math = require("math")
+-- }}}
 
+-- {{{ Function for drawing a single hand
+local function draw_hand(cr, m2, tmp_angle, is_hour, value, options)
+    -- Get the angle the context needs to be rotated by
+    local angle
+
+    if is_hour then
+        angle = ((value % 12) / 12) * 2 * math.pi
+    else
+        angle = ( value / 60) * 2       * math.pi
+    end
+
+    -- Get the width and height of the hand
+    local w = options.w
+    local h = options.h
+
+    -- Prepare for rotation
+    cr:translate(m2, m2)
+
+    -- Rotate back to original angle
+    cr:rotate(-tmp_angle)
+
+    -- Rotate to new angle
+    cr:rotate(angle)
+
+    -- Go back to original location
+    cr:translate(-m2, -m2)
+
+    -- Draw the hand
+    cr:set_source(gears.color(options.color))
+    cr:rectangle(m2 - w/2, m2 - h, w, h)
+
+    -- Fill the context
+    cr:fill()
+
+    -- Return new angle for next hand
+    return angle
+end
+-- }}}
+
+-- {{{ Function to draw all hands
+local function draw_hands(_, cr, width, height, options)
+    -- Make sure antialias is set to BEST
+    cr.antialias = cairo.Antialias.BEST
+
+    -- Variable definitions
+    local m2 = math.min(width, height) / 2
+    local tmp_angle = 0
+
+    -- Get the current time
+    local second = os.date('%S')
+    local minute = os.date('%M')
+    local hour   = os.date('%H')
+
+    -- Angle correction
+    local minute_new = (options.second.enable and (minute + second/60) or minute)
+    local hour_new   = (options.minute.enable and (hour   + minute/60) or hour  )
+
+    -- Draw hands
+    if options.second.enable then tmp_angle = draw_hand(cr, m2, tmp_angle, false, second,     options.second) end
+    if options.minute.enable then tmp_angle = draw_hand(cr, m2, tmp_angle, false, minute_new, options.minute) end
+    if options.hour.enable   then tmp_angle = draw_hand(cr, m2, tmp_angle, true,  hour_new,   options.hour  ) end
+end
+-- }}}
+
+-- {{{ Return function
 return function(options)
+    -- {{{ Create the widget
     local analog_clock = wibox.widget {
+        -- Set the contained widget to nil since we don't need one,
+        -- we draw everything in the bgimage function
         nil,
 
+        -- Function for drawing hands
         bgimage = function(_, cr, width, height)
-            cr.antialias = cairo.Antialias.BEST
-
-            local m = math.min(width, height)
-            local m2 = m / 2
-
-            local second = os.date('%S')
-            local minute = os.date('%M')
-            local hour   = os.date('%H')
-
-            local tmp_angle = 0
-
-            if options.second.enable then
-                local angle = (second / 60) * 2 * math.pi
-
-                local w = options.second.w
-                local h = options.second.h
-
-                cr:translate(m2, m2)
-                cr:rotate(angle)
-                cr:translate(-m2, -m2)
-                cr:set_source(gears.color(options.second.color))
-                cr:rectangle(m2 - w/2, m2 - h, w, h)
-
-                cr:fill()
-
-                tmp_angle = angle
-            end
-
-            if options.minute.enable then
-                local minute_new = (options.second.enable and (minute + second/60) or minute)
-                local angle = (minute_new / 60) * 2 * math.pi
-
-                local w = options.minute.w
-                local h = options.minute.h
-
-                cr:translate(m2, m2)
-                cr:rotate(-tmp_angle)
-                cr:rotate(angle)
-                cr:translate(-m2, -m2)
-                cr:set_source(gears.color(options.minute.color))
-                cr:rectangle(m2 - w/2, m2 - h, w, h)
-
-                cr:fill()
-
-                tmp_angle = angle
-            end
-
-            if options.hour.enable then
-                local hour_new = (options.minute.enable and (hour + minute/60) or hour)
-                local angle = ((hour_new % 12) / 12) * 2 * math.pi
-
-                local w = options.hour.w
-                local h = options.hour.h
-
-                cr:translate(m2, m2)
-                cr:rotate(-tmp_angle)
-                cr:rotate(angle)
-                cr:translate(-m2, -m2)
-                cr:set_source(gears.color(options.hour.color))
-                cr:rectangle(m2 - w/2, m2 - h, w, h)
-
-                cr:fill()
-
-                tmp_angle = angle
-            end
+            draw_hands(_, cr, width, height, options)
         end,
 
+        -- Background color and shape type
         bg = options.background.color,
         shape = options.background.shape,
 
+        -- ID and widget type
         id = 'analog_clock',
         widget = wibox.container.background
     }
+    -- }}}
 
+    -- {{{ Start the update timer
     gears.timer {
-        timeout = (
-            options.second.enable and 1
-            or (
-                options.minute.enable and 60
-                or (
-                    options.hour.enable and 3600
-                    or nil
-                )
-            )
-        ),
+        -- Get timeout based on requirements
+        timeout = ( options.second.enable and 1    or (
+                    options.minute.enable and 60   or (
+                    options.hour.enable   and 3600 or nil ))),
+
+        -- Timer options
         call_now = true,
         autostart = true,
+
+        -- Update the clock using redraw signal
         callback = function()
             analog_clock:emit_signal('widget::redraw_needed')
         end
     }
+    -- }}}
 
+    -- Return the widget
     return analog_clock
 end
+-- }}}
