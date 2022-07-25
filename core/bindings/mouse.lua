aa1e766e36086872b703ff848f2329dad782a303
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2022-07-25 09:50:32 -0700

    Add menu close to most mouse binds

--- core/bindings/mouse.lua
+++ core/bindings/mouse.lua
@@ -2,6 +2,7 @@ local awful = require("awful")
 
 -- {{{ Global mousebinds
 awful.mouse.append_global_mousebindings({
+    awful.button({ }, 1, function () Menu:hide() end),
     awful.button({ }, 3, function () Menu:toggle() end),
     awful.button({ }, 4, awful.tag.viewprev),
     awful.button({ }, 5, awful.tag.viewnext),
@@ -13,12 +14,15 @@ client.connect_signal("request::default_mousebindings", function()
     awful.mouse.append_client_mousebindings({
         awful.button({ }, 1, function (c)
             c:activate { context = "mouse_click" }
+            Menu:hide()
         end),
         awful.button({ Modkey }, 1, function (c)
             c:activate { context = "mouse_click", action = "mouse_move"  }
+            Menu:hide()
         end),
         awful.button({ Modkey }, 3, function (c)
             c:activate { context = "mouse_click", action = "mouse_resize"}
+            Menu:hide()
         end),
     })
 end)
