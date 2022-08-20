7b89ad63dc43291789e35dc66605b9726a2f052c
Apeiros-46B <Apeiros-46B@users.noreply.github.com>
2022-08-19 22:16:00 -0700

    fix: battery not showing charging when full

--- module/sysinfo/battery.lua
+++ module/sysinfo/battery.lua
@@ -3,13 +3,10 @@ local awful = require("awful")
 local interval = 3
 
 awful.widget.watch("acpi", interval, function(_, stdout)
-    local remaining = 0
-    local charging = false
+    local remaining = tonumber(string.match(stdout, "(%d?%d?%d)%%"))
 
-    remaining = tonumber(string.match(stdout, "(%d?%d?%d)%%"))
-    if (string.match(stdout, ": (%w+)") == "Charging") then
-        charging = true
-    end
+    local status = string.match(stdout, ": (%w+)")
+    local charging = (status == "Charging" or status == "Full")
 
     awesome.emit_signal("sysinfo::battery", remaining, charging)
 end)
